7d634b17b4775b9c5a46b59270a80ed3b4f527bf
rafaelBadan <rafael.badan@gmail.com>
2019-03-20 19:19:33 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
+import './config/DevToolsConfig';
+
+import Routes from './routes';
+
+const App = () => <Routes />;
+
+export default App;
